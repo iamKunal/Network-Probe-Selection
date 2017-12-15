4235a5983b7011bb225db621408a328b839c3904
Kunal Gupta <kunal.gupta@myself.com>
2017-12-15 16:01:04 +0530

    Added leftover stuff.

--- Balas/collect_data.py
+++ Balas/collect_data.py
@@ -4,31 +4,52 @@ from pwn import *
 
 import math
 
+import signal
+
+class TimeoutException(Exception):   # Custom exception class
+    pass
+
+def timeout_handler(signum, frame):   # Custom signal handler
+    raise TimeoutException
+
+# Change the behavior of SIGALRM
+signal.signal(signal.SIGALRM, timeout_handler)
+
+
 context.log_level='error'
 
-n = 4
+p = 25
 step = 1
-max_n = 40 + 1
+max_p = 35 + 1
 
-data=[]
+for k in xrange(27,36):
+    n=10
+    data=[]
 
-for i in xrange(n, max_n, step):
-    r = process('./bal.py')
+    for i in xrange(p, max_p, step):
+        r = process('./bal.py')
 
-    r.sendline('%d %d %d' %(i, i, math.ceil(i**0.5)+1))
-    cur=[i]
-    o = r.recvall().split('\n')
-    for line in o:
-        if('Time' in line or 'Iterations' in line):
-            cur.append(float(line.split()[-1]))
-    data.append(cur)
-    for d in data:
-        print d
-    r.kill()
+        r.sendline('%d %d %d' %(k,i, 7))
+        cur=[i]
+        signal.alarm(5)    
+        try:
+            o = r.recvall().split('\n')
+        except TimeoutException:
+            continue # continue the for loop if function A takes more than 5 second
+        else:
+            # Reset the alarm
+            signal.alarm(0)
+        for line in o:
+            if('Time' in line or 'Iterations' in line):
+                cur.append(float(line.split()[-1]))
+        data.append(cur)
+        for d in data:
+            print d
+        r.kill()
 
-with open('data/p_vs_iter_k3.csv', 'w') as f:
-    f.write('Number of Probes\tIterations\tSolution Time\tMatrix Time\n')
-    for d in data:
-        i,m,it,t=d
-        f.write('%d\t%d\t%f\t%f\n' %(i,it,t,m))
+    with open('data/p_vs_iter_p%d.csv' %(k), 'w') as f:
+        f.write('Number of Probes\tIterations\tSolution Time\tMatrix Time\n')
+        for d in data:
+            i,m,it,t=d
+            f.write('%d\t%d\t%f\t%f\n' %(i,it,t,m))
 
